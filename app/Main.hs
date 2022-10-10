f7010113413aa037856d5772871ef75004d115b1
user <user>
2022-10-10 10:41:39 -0500

    pemdas, tokens, whitespace, variables

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,25 @@ import Data.Char
 main :: IO ()
 main = putStrLn "fuck you"
 
+--Types
+
 data Expression = 
-    Num Char
+    Num Integer
+    | Var String
     | Add Expression Expression
     | Sub Expression Expression
+    | Mul Expression Expression
+    | Div Expression Expression
     deriving (Show)
 
 type Parser a = String -> Maybe (a, String) 
 
-data Assign = Assign Char Expression
+data Assign = Assign String Expression
     deriving Show
 
+--Infixes
+
+--Returns a parser if the predicate is true
 infix 7 <=>
 (<=>) :: Parser a -> (a -> Bool) -> Parser a  
 (parser <=> predicate) input = 
@@ -27,6 +35,7 @@ infix 7 <=>
                             then Just (a, rest) 
                             else Nothing
 
+--Either parser succeeds
 infixl 3 <|>
 (<|>) :: Parser a -> Parser a -> Parser a 
 (parserA <|> parserB) input = 
@@ -34,6 +43,7 @@ infixl 3 <|>
         Nothing -> parserB input
         result -> result
 
+--Both parsers succeed
 infixl 6 <+>
 (<+>) :: Parser a -> Parser b -> Parser (a, b)
 (parserA <+> parserB) input = 
@@ -43,6 +53,7 @@ infixl 6 <+>
             Nothing -> Nothing
             Just (resultB, cs) -> Just ((resultA, resultB), cs)
 
+--Both parsers succeeds; returns only left
 infixl 6 <+->
 (<+->) :: Parser a -> Parser b -> Parser a
 (parserA <+-> parserB) input = 
@@ -52,6 +63,7 @@ infixl 6 <+->
             Nothing -> Nothing
             Just (_, cs) -> Just (resultA, cs)
 
+--Both parsers succeeds; returns only right
 infixl 6 <-+>
 (<-+>) :: Parser a -> Parser b -> Parser b
 (parserA <-+> parserB) input = 
@@ -61,6 +73,7 @@ infixl 6 <-+>
             Nothing -> Nothing
             Just (resultB, cs) -> Just (resultB, cs)
 
+--Applies the function (transformer) if the parser succeeds e.g. (char >>> toUpper) "a"
 infixl 5 >>>
 (>>>) :: Parser a -> (a -> b) -> Parser b 
 (parser >>> transformer) input = 
@@ -75,12 +88,21 @@ infix 4 +>
         Nothing -> Nothing
         Just (a, cs) -> function a cs
 
+--Parsers
+
 char :: Parser Char
 char [] = Nothing
 char (x:xs) = Just (x, xs)
 
 digit :: Parser Char
 digit = char <=> isDigit
+
+digits :: Parser String
+digits = iter digit
+
+number :: Parser Integer
+number = literal '-' <-+> digits >>> (\n -> -1 * (read n :: Integer))
+        <|> digits >>> (\n -> read n :: Integer)
 
 space :: Parser Char
 space = char <=> isSpace
@@ -91,30 +113,53 @@ letter = char <=> isAlpha
 literal :: Char -> Parser Char
 literal c = char <=> (==c)
 
+letters :: Parser String
+letters = iter letter
+
+token :: Parser a -> Parser a
+token = (<+-> iterS space)
+
+iter :: Parser Char -> Parser String
+iter m = (iterS m) <=> (/="")
+iterS m = m <+> iterS m >>> (\(x,y) -> x:y)
+        <|> result []
+
+--Operators
+
+addOp :: Parser (Expression -> Expression -> Expression)
+addOp = token(literal '+') >>> (\_ -> Add)
+    <|> token(literal '-') >>> (\_ -> Sub)
+
+expression :: Parser Expression
+expression = token(term) +> expression'
+expression' e = addOp <+> term >>> buildOp e +> expression'
+                      <|> result e
+
+mulOp :: Parser (Expression -> Expression -> Expression)
+mulOp = token(literal '*') >>> (\_ -> Mul)
+    <|> token(literal '/') >>> (\_ -> Div)
+
+term :: Parser Expression
+term = token(factor) +> term'
+term' e = mulOp <+> term >>> buildOp e +> term'
+                <|> result e
+
+factor :: Parser Expression
+factor = token (literal '(') <-+> token(expression) <+-> token(literal ')')
+        <|> number >>> Num
+        <|> letters >>> Var
+
+buildOp :: Expression -> ((Expression -> Expression -> Expression), Expression) -> Expression
+buildOp expressionA (op, expressionB) = op expressionA expressionB
+
 parse :: String -> Assign
 parse s = Assign id expr
     where (id, expr) = case assign s of
                             Nothing -> error "Invalid assignemnt"
                             Just ((a, b), _) -> (a, b)
 
-assign :: Parser (Char, Expression)
-assign = letter <+-> literal '=' <+> expression
-
-term :: Parser Expression
-term = digit >>> Num
+assign :: Parser (String, Expression)
+assign = token(letters) <+-> token(literal '=') <+> expression
 
 result :: a -> Parser a 
 result a cs = Just (a, cs)
-
-expression :: Parser Expression
-expression = term +> expression'
-
-expression' e = addOp <+> term >>> buildOp e +> expression'
-                <|> result e
-
-addOp :: Parser (Expression -> Expression -> Expression)
-addOp = literal '+' >>> (\_ -> Add)
-    <|> literal '-' >>> (\_ -> Sub)
-
-buildOp :: Expression -> ((Expression -> Expression -> Expression), Expression) -> Expression
-buildOp expressionA (op, expressionB) = op expressionA expressionB
